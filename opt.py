022101760bfd390864dd3e479e9941147e164877
v-sion-v <m15010037956@gmail.com>
2025-04-03 19:42:39 +0800

    VGTSR 1.2

--- opt.py
+++ opt.py
@@ -3,18 +3,18 @@
 # Data
 train_dataset_path = 'dataset/VGTSR/train'
 eval_dataset_path = 'dataset/VGTSR/eval'
-lr_dir_name = 'LR thermal/4/BI'
+lr_dir_name = 'LR thermal/8/BD'
 guide_dir_name = 'HR RGB'
 hr_dir_name = 'GT thermal'
 HR_image_size = (512, 640)
 
 # Model
 model_name = 'GSRNet'
-batch_size = 4
+batch_size = 2
 window_size = (8, 10)
-num_self_attention_layers = 1
+num_self_attention_layers = 0
 num_cross_attention_layers = 1
-num_reconstruction_layers = 1
+num_reconstruction_layers = 0
 num_head_list = [4, 8, 16, 32]
 num_channels_list = [64, 128, 256, 512]
 num_conv_down_layers_list = [2, 2, 2, 2]
@@ -31,16 +31,16 @@ gradient_loss_weight = 0.1
 
 # Train
 learning_rate = 0.0001
-epochs = 200
+epochs = 100
 print_loss_in_one_epoch = 20
 save_model_epoch = 1
 checkpoints_dir = 'checkpoints'
 progressive = False
 start_scale = 1
 tensorboard_log_dir = 'tensorboard_log'
-lr_decay_step = 10
+lr_decay_step = 16
 lr_decay_rate = 0.5
-data_parallel = True
+data_parallel = False
 
 # Device
 gpu = torch.device('cuda:0') # Set to cuda:0 in DataParallel
